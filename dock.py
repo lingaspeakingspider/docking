2b84870e8c26f9c1acdd2e9cbe964f9b678b88c3
oguzkagan <116112769+oguz169kagan@users.noreply.github.com>
2023-06-12 13:10:35 +0300

    Add files via upload

---
+++ dock.py
@@ -0,0 +1,33 @@
+from ccdc.docking import Docker
+from ccdc.io import MoleculeReader, EntryReader
+import tempfile
+import os
+
+docker = Docker()
+settings = docker.settings
+
+MLL1_protein_file = "protein dosyası"
+
+settings.add_protein_file(MLL1_protein_file)
+
+# #*****************************
+# #Binding Site belirleme kısmı (isteğe bağlı)
+# MLL1_native_ligand_file = ""#<proteinde bulunan hazır ligandın dosyası> string olarak yaz
+
+# MLL1_native_ligand = MoleculeReader(MLL1_native_ligand_file)[0]
+# MLL1_protein = settings.proteins[0]
+# settings.binding_site = settings.BindingSiteFromLigand(MLL1_protein, MLL1_native_ligand, 8.0)
+# #*****************************
+
+MLL1_ligand_file = 'ligand dosyası'
+MLL1_ligand = MoleculeReader(MLL1_ligand_file)[0]
+settings.add_ligand_file(MLL1_ligand_file, 10)
+
+os.chdir("/home/biocally/Desktop")
+settings.fitness_function = 'plp'
+settings.autoscale = 10.
+settings.early_termination = False
+batch_tempd = tempfile.mkdtemp()
+settings.output_directory = batch_tempd
+settings.output_file = "output dosyasının adı"
+
