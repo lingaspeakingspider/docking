ec1edda026495f5b8b9e737890139ce9cbce2045
oguzkagan <116112769+oguz169kagan@users.noreply.github.com>
2023-06-12 15:46:09 +0300

    Update pyrosettafile.py

--- pyrosettafile.py
+++ pyrosettafile.py
@@ -1,12 +1,20 @@
 from pyrosetta import *
 from pyrosetta.toolbox import *
-
+from dock import docking
+import random
 init()
 
-model=pose_from_pdb("<protein>")
+index_of_mutate_amino=0
+
+aminoacid_list=['M', 'P', 'A', 'V', 'G', 'Y', 'T', 'D', 'W', 'I', 'F', 'S', 'K', 'E', 'H', 'L', 'N', 'R', 'C', 'Q']
+random_amino = random.choice(aminoacid_list)
+
+model = pose_from_pdb("<protein>")
 
 seq=pose.sequence()
 print(seq)
 
-#1-) specify amino acids
-#2-) apply mutations
+mutate_residue(pose, index_of_mutate_amino, random_amino)
+
+
+
