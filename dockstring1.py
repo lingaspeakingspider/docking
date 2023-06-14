e962433c08a1b07413d36e21be5d42f95b102dbe
oguzkagan <116112769+oguz169kagan@users.noreply.github.com>
2023-06-14 13:30:39 +0300

    Update dockstring1.py

--- dockstring1.py
+++ dockstring1.py
@@ -2,7 +2,7 @@ from dockstring import load_target
 
 import rdkit.Chem as Chem
 
-smiles = 'CC1=C(C(=O)N2CCCCC2=N1)CCN3CCC(CC3)C4=NOC5=C4C=CC(=C5)F'
+smiles = "<smiles of ligand>"
 Chem.MolFromSmiles(smiles)
 
 target = load_target('DRD2')
