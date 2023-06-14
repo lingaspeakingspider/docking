e524fe605b7d7a5e9009672f1fea2615591cac67
oguzkagan <116112769+oguz169kagan@users.noreply.github.com>
2023-06-14 18:41:16 +0300

    Update pyrosettafile.py

--- pyrosettafile.py
+++ pyrosettafile.py
@@ -1,20 +1,19 @@
 from pyrosetta import *
 from pyrosetta.toolbox import *
-from dock import docking
-import random
+from dockstring1 import docking
 init()
 
 index_of_mutate_amino=0
 
 aminoacid_list=['M', 'P', 'A', 'V', 'G', 'Y', 'T', 'D', 'W', 'I', 'F', 'S', 'K', 'E', 'H', 'L', 'N', 'R', 'C', 'Q']
-random_amino = random.choice(aminoacid_list)
 
 model = pose_from_pdb("<protein>")
+for i in aminoacid_list:
+  seq=pose.sequence()
+  print(seq)
 
-seq=pose.sequence()
-print(seq)
-
-mutate_residue(pose, index_of_mutate_amino, random_amino)
+  mutate_residue(pose, index_of_mutate_amino, random_amino)
+  docking(
 
 #Nimm anhand eines Formular, das in VS Code liegt, an, wie wir die Unterlagen in PyRosetta speichern
 
