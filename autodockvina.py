edfced83b4f306a3883f223d3ae9997ea418dfa9
oguzkagan <116112769+oguz169kagan@users.noreply.github.com>
2023-06-20 15:16:38 +0300

    Update autodockvina.py

--- autodockvina.py
+++ autodockvina.py
@@ -1,5 +1,5 @@
 from vina import Vina
-
+import os
 
 v = Vina(sf_name='vina')
 
@@ -13,11 +13,11 @@ v.compute_vina_maps(center=[15.190, 53.903, 16.917], box_size=[20, 20, 20])
 # print('Score before minimization: %.3f (kcal/mol)' % energy[0])
 
 # Minimized locally the current pose
-energy_minimized = v.optimize()
-print('Score after minimization : %.3f (kcal/mol)' % energy_minimized[0])
-v.write_pose('1iep_ligand_minimized.pdbqt', overwrite=True)
+# energy_minimized = v.optimize()
+# print('Score after minimization : %.3f (kcal/mol)' % energy_minimized[0])
+# v.write_pose('1iep_ligand_minimized.pdbqt', overwrite=True)
 
 # Dock the ligand
 v.dock(exhaustiveness=32, n_poses=20)
-v.score()
+os.chdir("/home/biocally/Desktop")
 v.write_poses('<output>', n_poses=5, overwrite=True)
